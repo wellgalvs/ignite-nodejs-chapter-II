9380f019de29b8fe55d1345eadb9dcab24e83dd1
Wellington G. Alves da Silva <wellingtonalvs@gmail.com>
2022-04-25 17:19:19 -0300

    Node JS Chapter II

---
+++ 1-typescript/src/routes.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(request: Request, response: Response) {
+  CreateCourseService.execute({
+    name: "NodeJs",
+    duration: 10,
+    educator: "Wellington"
+  });
+  return response.send();
+}
